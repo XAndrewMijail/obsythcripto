16063e247ff06205874fc261731a07c55f288f86
XAndrewMijail <xrm.andrew.mijail@icloud.com>
2026-01-20 11:12:52 -0400

    Actualizar nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  routeRules: {
-    // prerender index route by default
-    '/': { prerender: true },
-  },
-});
+devtools: { enabled: true },
+
+app: {
+head: {
+title: ‘Obsyth - Descubre Wallets Perdidas’,
+meta: [
+{ charset: ‘utf-8’ },
+{ name: ‘viewport’, content: ‘width=device-width, initial-scale=1’ },
+{
+name: ‘description’,
+content: ‘Comando CMD profesional para búsqueda de wallets inactivas con saldo en Bitcoin, Ethereum y BNB’
+},
+{ name: ‘theme-color’, content: ‘#000000’ }
+],
+link: [
+{ rel: ‘icon’, type: ‘image/x-icon’, href: ‘/favicon.ico’ }
+]
+}
+},
+
+css: [’~/assets/css/main.css’],
+
+modules: [],
+
+compatibilityDate: ‘2025-01-20’
+})
